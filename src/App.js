6b4627e3c94c7b2ea772d099f02b0cddf61c640f
cs862 <59978170+cs862@users.noreply.github.com>
2024-10-26 16:51:53 +0100

    UI improvements and additional data points

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Books from "./components/Books";
 import BookDetails from "./components/BookDetails";
 import bookData from "./data/books.json";
@@ -9,7 +8,6 @@ function App() {
   return (
     <Router>
       <div className="App lg:px-8">
-        <Navbar />
         <Routes>
           <Route path="/book-notes" element={<Books />} />
           <Route path="/books/:id" element={<BookDetails books={bookData} />} />
--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,41 +1,40 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const BookCard = ({ book }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col md:flex-row">
-      <div className="mb-4 md:mb-0 w-full md:w-1/2">
+    <Link
+      to={`/books/${book.id}`}
+      className="bg-white shadow-lg rounded-lg p-6 flex flex-col hover:cursor-pointer group"
+    >
+      <div className="relative mb-4 w-full">
         <img
           src={book.image}
           alt={book.title}
           className="rounded-lg w-full object-cover h-[30vh]"
         />
-      </div>
-      <div className="flex-grow pl-0 md:pl-6 flex flex-col justify-between">
-        <div className="text-center md:text-left">
-          <div className="text-2xl text-primaryblue font-karla font-bold mb-4">
-            {book.title}
-          </div>
-          <p className="mt-2 text-gray-600 font-roboto">{book.summary}</p>
-        </div>
-        <div className="flex items-center justify-center md:justify-end space-x-4 mt-4">
-          <Link
-            to={`/books/${book.id}`}
-            className="text-white rounded-lg border cursor-pointer bg-primaryblue border-primary-600 hover:bg-secondaryblue py-2 px-3 text-xs font-medium text-center"
-          >
-            Notes
-          </Link>
-          <a href={book.amazon_link} target="_blank">
-            <FontAwesomeIcon
-              icon={faAmazon}
-              className="self-center text-darkgrey text-lg sm:text-xl transition-colors duration-100 hover:text-primaryblue"
-            />
-          </a>
+        {/* Overlay for type */}
+        <div className="absolute top-2 right-2 rounded-lg px-2 py-1 text-gray-600 transition-colors duration-200 bg-gray-200 group-hover:bg-gray-300 text-xs font-bold">
+          {book.type}
         </div>
       </div>
-    </div>
+      <div className="flex-grow text-center">
+        <div className="text-2xl text-primaryblue font-karla font-bold mb-4">
+          {book.title}
+        </div>
+        <p className="mt-2 text-gray-600 font-roboto">{book.summary}</p>
+      </div>
+      <div className="flex items-center justify-end space-x-4 mt-4">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full transition-colors duration-200 bg-white group-hover:bg-black">
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            className="text-black transition-colors duration-200 group-hover:text-white"
+          />
+        </div>
+      </div>
+    </Link>
   );
 };
 
